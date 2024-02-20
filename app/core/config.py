48bbfdc32cd5dafca377b1979034a1d9aca5f6ba
Timothy-py <adeyeyetimothy33@gmail.com>
2024-02-20 13:49:19 +0100

    fx

--- app/core/config.py
+++ app/core/config.py
@@ -7,10 +7,6 @@ class Settings(BaseSettings):
 	DATABASE_URL: str
 	JWT_KEY_ID: str
 	JWT_SIGNATURE_ALGORITHM: str
-	FLUTTERWAVE_PUBLIC_KEY: str
-	FLUTTERWAVE_SECRET_KEY: str
-	FLUTTERWAVE_ENCRYPTION_KEY: str
-	WAVE_BASE_URL: str
 
 
 	model_config = SettingsConfigDict(env_file=".env")
